c47bc1bd551bb6c6985e0dc46d2533af5640f366
Harold <harol.mont97@gmail.com>
2019-09-10 22:38:43 -0500

    se actualizan las rutas y los controllers de tareas

--- src/controllers/tarea.controller.js
+++ src/controllers/tarea.controller.js
@@ -18,10 +18,77 @@ export async function create_tarea(req, res){
         })
     }
 }
+export async function get_tarea(req,res){
+    try{
+        const tareas = await tarea.findAll();
+        res.json({tareas})
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+            message:'Something goes wrong'
+        })
+    }
+}
+export async function get_tareaid(req,res){
+    try{
+        const{id} = req.params;
+        const tareadb = await tarea.findAll({
+            attribute:['descripcion','subclase_accesorio_id','fecha_inicio','fecha_fin'],
+            where: {id}
+        });
+        res.json({
+            tareadb
+        });
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+            message:'Something goes wrong'
+        })
+    }
+}
+export async function update_tarea(req,res){
+    try{
+        const {id} = req.params;
+        const {descripcion,subclase_accesorio_id,fecha_inicio,fecha_fin} = req.body;
+        const newTarea = await tarea.findAll({
+            attribute:['descripcion','subclase_accesorio_id','fecha_inicio','fecha_fin'],
+            where: {id}
+        });
 
-export async function consultar_tarea(req,res){
-    try{}
-    catch(err){
-        
+        if(newTarea.length > 0){
+            newTarea.forEach(async element =>{
+                await element.update({descripcion,subclase_accesorio_id,fecha_inicio,fecha_fin});
+            });
+        }
+        return res.json({
+            message: 'Project update successfully',
+            data: newTarea
+        })
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+            message:'Something goes wrong'
+        })
+    }
+}
+export async function delete_tarea(req,res){
+    try{
+        const {id}= req.params;
+        const tareadb = await tarea.destroy({
+            where:{id}
+        });
+        res.json({
+            message: 'tarea eliminados',
+            cantidad: tareadb
+        })
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({
+            message:'Something goes wrong'
+        })
     }
 }
--- src/routes/tarea.routes.js
+++ src/routes/tarea.routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 const router = Router();
 
-import { create_tarea } from '../controllers/tarea.controller';
+import { create_tarea, get_tarea, get_tareaid, update_tarea, delete_tarea} from '../controllers/tarea.controller';
 
 // /api/subclase-accesorio
 router.post('/',(create_tarea))
+router.get('/',(get_tarea))
+router.get('/:id',(get_tareaid))
+router.put('/:id',(update_tarea))
+router.delete('/:id',(delete_tarea))
+
 
 export default router;
